d77069239a8943e064b884c83cc0b5a57701df66
Ali Naji <ali-naji@users.noreply.github.com>
2020-04-23 13:31:26 -0700

    modified management

--- packages/regression_model/regression_model/processing/data_management.py
+++ packages/regression_model/regression_model/processing/data_management.py
@@ -42,7 +42,6 @@ def save_pipeline(*, pipeline_to_persist) -> None:
 
 def load_pipeline(*, file_name: str) -> Pipeline:
     """Load a persisted pipeline."""
-
-    trained_model = joblib.load(filename=Path(
-        os.environ.get('pipeline_save_path')))
+    file_path = Path(os.environ.get('pipeline_save_path'))
+    trained_model = joblib.load(filename=file_path)
     return trained_model
